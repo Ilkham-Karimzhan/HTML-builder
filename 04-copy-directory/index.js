1aec787a51134d7ddf3397d3d8cf8d7b5c638ca8
Ilkham-Karimzhan <ilhamkar00@gmail.com>
2023-05-06 20:21:00 +0600

    complete: implement copy-derictory function (Sat, May  6, 2023  8:21:00 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,44 @@
+const { error } = require("console");
+const fs = require("fs");
+const path = require("path");
 
+fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true }, (error) => {
+  if (error) return error.message;
+  console.log("Директория создана!");
+});
+
+fs.readdir(
+  path.join(__dirname, "files"),
+  { withFileTypes: true },
+  (error, data) => {
+    if (error) return error.message;
+
+    data.forEach((item) => {
+      if (item.isDirectory()) return;
+      const filePath = path.join(__dirname, "files", item.name);
+      fs.stat(filePath, (error, stats) => {
+        if (error) return console.log(error);
+        fs.readFile(path.join(__dirname, "files", item.name), (error, data) => {
+          if (error) return console.log(error.message);
+
+          fs.writeFile(
+            path.join(
+              __dirname,
+              "files-copy",
+              item.name.split(".")[0] + path.extname(item.name)
+            ),
+            data,
+            (error) => {
+              if (error) return console.log(error.message);
+              console.log(
+                `Файл ${
+                  item.name.split(".")[0] + path.extname(item.name)
+                } скопирован`
+              );
+            }
+          );
+        });
+      });
+    });
+  }
+);
